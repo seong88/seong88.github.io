0d597678666b46d632ae6e2de1ea7c7ed4dc868d
Seong Yoo <Sryeong88@gmail.com>
2017-04-25 09:46:55 -0700

    asset change

--- info.js
+++ info.js
@@ -1,5 +1,5 @@
     $(document).ready(function() {
-        $("#phone").html("213-587-1855");
-        $("#email").html("sryeong88@gmail.com");
+        $("#phone").html(" 213-587-1855");
+        $("#email").html("<a href='mailto:sryeong88@gmail.com'>sryeong88@gmail.com</a>");
         
     });
